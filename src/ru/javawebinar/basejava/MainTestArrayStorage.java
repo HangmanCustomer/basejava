300c275749d943b68d67acd9b8736717ec75e686
HangmanCustomer <anotherdawnrises@gmail.com>
2018-07-06 20:46:22 +0300

    one more delete fix

--- src/ru/javawebinar/basejava/MainTestArrayStorage.java
+++ src/ru/javawebinar/basejava/MainTestArrayStorage.java
@@ -42,7 +42,6 @@ public class MainTestArrayStorage {
         SORTED_ARRAY_STORAGE.save(r3);
         SORTED_ARRAY_STORAGE.save(r2);
 
-
         System.out.println("Get r1: " + SORTED_ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + SORTED_ARRAY_STORAGE.size());
 
