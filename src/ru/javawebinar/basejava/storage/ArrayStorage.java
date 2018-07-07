fd1d82e758e4a1c5116f97c6e46abd042cf15394
HangmanCustomer <anotherdawnrises@gmail.com>
2018-07-07 15:50:15 +0300

    before lesson 04

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -7,6 +7,17 @@ import ru.javawebinar.basejava.model.Resume;
  */
 public class ArrayStorage extends AbstractArrayStorage {
 
+    @Override
+    protected void fillDeletedElement(int index) {
+        storage[index] = storage[size - 1];
+    }
+
+    @Override
+    protected void insertElement(Resume r, int index) {
+        storage[size] = r;
+    }
+
+    @Override
     protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
@@ -14,16 +25,5 @@ public class ArrayStorage extends AbstractArrayStorage {
             }
         }
         return -1;
-    }
-
-    @Override
-    protected void directSave(Resume resume, int index) {
-        storage[size] = resume;
-    }
-
-    @Override
-    protected void directDelete(int index) {
-        storage[index] = storage[size - 1];
-        storage[size - 1] = null;
     }
 }
--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -1,33 +1,31 @@
 package ru.javawebinar.basejava.storage;
 
-
 import ru.javawebinar.basejava.model.Resume;
-
 
 import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
-
-    protected int getIndex(String uuid) {
-        Resume searchKey = new Resume();
-        searchKey.setUuid(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
-    }
-
-
     @Override
-    protected void directSave(Resume resume, int index) {
-        if (index < 0) {
-            index = Math.abs(index) - 1;
-            System.arraycopy(storage, index, storage, index + 1, size - index);
-            storage[index] = resume;
+    protected void fillDeletedElement(int index) {
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(storage, index + 1, storage, index, numMoved);
         }
     }
 
     @Override
-    protected void directDelete(int index) {
-        System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
-        storage[size - 1] = null;
+    protected void insertElement(Resume r, int index) {
+//      http://codereview.stackexchange.com/questions/36221/binary-search-for-inserting-in-array#answer-36239
+        int insertIdx = -index - 1;
+        System.arraycopy(storage, insertIdx, storage, insertIdx + 1, size - insertIdx);
+        storage[insertIdx] = r;
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        Resume searchKey = new Resume();
+        searchKey.setUuid(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
     }
 }
