2f0ea4fcdb9760a274d75cd9f2e195c3583168db
HangmanCustomer <anotherdawnrises@gmail.com>
2018-09-17 21:20:21 +0300

    HW lesson 07 clean code

--- src/ru/javawebinar/basejava/model/SectionType.java
+++ src/ru/javawebinar/basejava/model/SectionType.java
@@ -1,9 +1,5 @@
 package ru.javawebinar.basejava.model;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.TreeMap;
-
 public enum SectionType {
     //hope it will be on a github
     PERSONAL("-Личные качества"),
@@ -13,46 +9,6 @@ public enum SectionType {
     EXPERIENCE("-Опыт работы"),
     EDUCATION("-Образование");
 
-    },
-    ACHIEVEMENT("-Достижения") {
-        @Override
-        public void getSectionInfo() {
-            ArrayList<String> list = new ArrayList<>();
-            list.add("Достижение 01");
-            list.add("Достижение 02");
-            list.forEach(System.out::println);
-        }
-
-    },
-    QUALIFICATIONS("-Квалификация") {
-        @Override
-        public void getSectionInfo() {
-            ArrayList<String> list = new ArrayList<>();
-            list.add("Технология 01");
-            list.add("Технология 02");
-            list.forEach(System.out::println);
-        }
-    },
-    EXPERIENCE("-Опыт работы") {
-        @Override
-        public void getSectionInfo() {
-            Map<String, String> map = new TreeMap<>();
-            map.put("Компания 01", "Должность и задачи");
-            map.put("Компания 02", "Должность и задачи");
-            map.put("Компания 03", "Должность и задачи");
-            map.forEach((key, value) -> System.out.println(key + " " + value));
-        }
-    },
-    EDUCATION("-Образование") {
-        @Override
-        public void getSectionInfo() {
-            Map<String, String> map = new TreeMap<>();
-            map.put("Конференция 01", "Знания");
-            map.put("Конференция 02", "Знания");
-            map.put("Университет", "время обучения");
-            map.forEach((key, value) -> System.out.println(key + " " + value));
-        }
-    };
     private String title;
 
     SectionType(String title) {
@@ -64,5 +20,4 @@ public enum SectionType {
         return title;
     }
 
-   // abstract Object typePrint(Object o);
 }
